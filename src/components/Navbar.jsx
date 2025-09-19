64122014a6aa269b376b768bce03201852b42c31
unknown <singhxamit21@example.com>
2025-09-19 21:13:43 +0530

    hotfix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
-export default function Navbar({...props}) {
+export default function Navbar({ ...props }) {
     const { route, setRoute } = props;
-  return (
-    <nav className="navbar">
-      <div className="font-bold text-lg">Shipping Box</div>
-      <div>
-        <button
-          onClick={() => setRoute('form')}
-          className="btn"
-          style={route === 'form' ? { background: '#1d4ed8' } : {}}
-        >
-          Add Box
-        </button>
-        <button
-          onClick={() => setRoute('list')}
-          className="btn"
-          style={route === 'list' ? { background: '#1d4ed8' } : {}}
-        >
-          List Boxes
-        </button>
-      </div>
-    </nav>
-  )
+    return (
+        <nav className="navbar">
+            <div className="font-bold text-lg">Shipping Box</div>
+            <div>
+                <button
+                    onClick={() => setRoute('form')}
+                    className="btn"
+                    style={route === 'form' ? { background: '#1d4ed8' } : {}}
+                >
+                    Add Box
+                </button>
+                <button
+                    onClick={() => setRoute('list')}
+                    className="btn"
+                    style={route === 'list' ? { background: '#1d4ed8' } : {}}
+                >
+                    List Boxes
+                </button>
+            </div>
+        </nav>
+    )
 }
